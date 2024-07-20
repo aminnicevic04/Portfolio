c86c6a70004f3d06b4a04d6078f038b04fc50f20
aminnicevic04 <aminnicevic@gmail.com>
2024-07-20 15:32:04 +0200

    update

--- src/components/Ui/Welcome.js
+++ src/components/Ui/Welcome.js
@@ -7,7 +7,7 @@ const Welcome = () => {
       <h1 className="wl_text">
         <div className="message">
           <div className="word1">Hello!</div>
-          <div className="word2">Hola!</div>
+          <div className="word2">Bonjour!</div>
           <div className="word3">Zdravo!</div>
         </div>
       </h1>
